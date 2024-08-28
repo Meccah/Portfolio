10a121f5f8123a68d5a2a21b70495c4ff19c4f36
meccah <georgekimiti79@gmail.com>
2024-08-28 16:50:41 +0300

    message

--- port/src/components/Content.vue
+++ port/src/components/Content.vue
@@ -1,33 +1,62 @@
 <template>
   <div>
-<!-- third section -->
-<div class="bg-gray-700">
-        <div class="max-w-5xl mx-auto flex flex-col gap-5 py-5">
-          <h1 class="px-5 py-3">
-            <span class="text-white text-4xl">Projects</span>
-          </h1>
-          <div class="lg:w-[1000px] h-48 bg-blue-950 rounded-xl mx-3 px-4 py-3">
-            <h3 class="text-gray-200 text-3xl">Websites</h3>
-            <p class="text-gray-500 text-xl">Seasonal Jobs</p>
-            <p class="text-gray-500 text-xl">Profia Institute</p>
+    <!-- third section -->
+    <div class="bg-gray-700">
+      <div class="max-w-5xl mx-auto flex flex-col gap-5 py-5">
+        <h1 class="px-5 py-3">
+          <span class="text-white text-4xl">Projects</span>
+        </h1>
+        <div
+          class="lg:w-[900px] h-48 lg:mx-auto bg-blue-950 rounded-xl mx-3 px-4 py-3"
+        >
+          <h3 class="text-gray-200 text-3xl">Websites</h3>
+          <div>
+            <p class="text-gray-500 text-xl">
+              Seasonal Jobs
+              <span class="pl-28 text-sm text-blue-400">
+                <a href="https://seasonal-jobs.vercel.app/"
+                  >https://seasonal-jobs.vercel.app/</a
+                ></span
+              >
+            </p>
+            <p class="text-gray-500 text-xl">
+              Profia Institute
+              <span class="pl-20 text-sm text-blue-400">
+                <a href="https://profia-institute-three.vercel.app/"
+                  >https://profia-institute-three.vercel.app/</a
+                ></span
+              >
+            </p>
           </div>
-          <div class="lg:w-[1000px] h-48 bg-blue-950 rounded-xl mx-3">
-
-          </div>
-          <div class="lg:w-[1000px] h-48 bg-blue-950 rounded-xl mx-3 px-4 py-3">
-            <h3 class="text-gray-200 text-3xl">Languages</h3>
-
-
+        </div>
+        <div
+          class="lg:w-[900px] h-48 lg:mx-auto bg-blue-950 rounded-xl mx-3"
+        ></div>
+        <div
+          class="lg:w-[900px] h-48 lg:mx-auto bg-blue-950 rounded-xl mx-3 px-4 py-3"
+        >
+          <h3 class="text-gray-200 text-3xl text-center">Languages</h3>
+          <div class="flex justify-center items-center gap-20 text-lg">
+            <div class="text-gray-100 pt-8">
+              <h4>First Languages</h4>
+              <p class="text-gray-400">English</p>
+              <p class="text-gray-400">Kiswahili</p>
+            </div>
+            <div class="text-gray-100 pt-2">
+              <h4>Secondary languages</h4>
+              <p class="text-gray-400">Gikuyu</p>
+            </div>
+            <div class="text-gray-100 pt-2">
+              <h4>Tertiary Languages</h4>
+              <p class="text-gray-400">Japanese</p>
+            </div>
           </div>
         </div>
       </div>
+    </div>
   </div>
 </template>
 
-<script setup>
+<script setup></script>
 
-</script>
-
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
